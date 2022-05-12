9db1c519d2e6316f3fb3610f9f83547c7f9733a8
Artsiom Kaliaha <stakkato95@gmail.com>
2022-05-12 12:11:29 +0200

    Homework 2. Add consensus

--- 2/SimpleBlockchain/app.py
+++ 2/SimpleBlockchain/app.py
@@ -64,5 +64,47 @@ def mine():
     return jsonify(response), 200
 
 
+###################################################
+# consensus
+###################################################
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+
+    if nodes is None:
+        return "Error: Invalid data", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
+###################################################
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0 ', port=5000)
--- 2/SimpleBlockchain/blockchain.py
+++ 2/SimpleBlockchain/blockchain.py
@@ -2,6 +2,8 @@ import hashlib
 import json
 from time import time
 
+import requests as requests
+
 
 class Blockchain(object):
 
@@ -10,6 +12,7 @@ class Blockchain(object):
         self.current_transactions = []
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+        self.nodes = set()
 
     def new_block(self, proof, previous_hash=None):
         # Creates a new Block and adds it to the chain
@@ -54,6 +57,63 @@ class Blockchain(object):
         while self.valid_proof(last_proof, proof) is False:
             proof += 1
         return proof
+
+    ###################################################
+    # consensus
+    ###################################################
+
+    def valid_chain(self, chain):
+        # Determine if a given blockchain is valid
+        # : param chain : <list > A blockchain
+        # : return : <bool > True if valid , False if not
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+    def register_node(self, node):
+        self.nodes.add(node)
+
+    ###################################################
 
     @property
     def last_block(self):
