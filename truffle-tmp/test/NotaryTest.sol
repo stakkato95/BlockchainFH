c365fabbd49579780e3d690882eec331fa1e75d4
Artsiom Kaliaha <stakkato95@gmail.com>
2022-05-28 19:37:21 +0200

    Solidity learning. Update solidity test

--- truffle-tmp/test/NotaryTest.sol
+++ truffle-tmp/test/NotaryTest.sol
@@ -1,10 +1,26 @@
 pragma solidity ^0.4.24;
 
 import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Notary.sol";
 
 contract NotaryTest {
 
-    function testAddAndRead() public {
+    bytes32 constant checksum = 0x9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08;
+    string constant filename = "testFile";
+    string constant comment = "test comment";
 
+    function testAddAndRead() public {
+        Notary notaryContract = Notary(DeployedAddresses.Notary());
+        notaryContract.addEntry(checksum, filename, comment);
+
+        string memory actualFilename;
+        uint timestamp;
+        string memory actualComment;
+        address actualSender;
+        (actualFilename, timestamp, actualComment, actualSender) = notaryContract.getEntry(checksum);
+        Assert.equal(actualFilename, filename, "wrong filename");
+        Assert.equal(actualComment, comment, "wrong comment");
+        Assert.equal(actualSender, address(this), "wrong sender");
     }
 }
