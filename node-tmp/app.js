2705eddbe88fd0530d706afebe3555098c0f1d4e
Artsiom Kaliaha <stakkato95@gmail.com>
2022-05-29 20:45:57 +0200

    Solidity learning. Add endpoint to add entry

--- node-tmp/app.js
+++ node-tmp/app.js
@@ -3,18 +3,88 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
+var Web3 = require("web3")
+var Web3Quorum = require("web3js-quorum")
+var web3 = new Web3Quorum(new Web3("http://localhost:8545"))
+
+const Tx = require("ethereumjs-tx").Transaction;
+const Common = require('@ethereumjs/common').default
+const customCommon = Common.forCustomChain(
+    'mainnet',
+    {
+        name: 'k8s',
+        networkId: 1981,
+        chainId: 1981,
+    },
+    'istanbul',
+);
+
+const ABI = require("./abi.json")
+
+const fromAccountPublicKey = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73"
+const fromAccountPrivateKey = "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"
+const deployedContractAddress = "0xf90E97357b8dF646389519112F2fdeE1f8A09bd0"
+var notaryInstace = new web3.eth.Contract(ABI, deployedContractAddress)
+
+const addEntryAbi = notaryInstace._jsonInterface.find((e) => {
+    return e.name === "addEntry";
+});
+
 app.get('/chain/entry/:hash', (req, res) => {
     const hash = req.params['hash']
     res.send(`Hello World! ${hash}`)
 })
 
-app.post('/chain/entry', (req, res) => {
+
+
+app.post('/chain/entry', async (req, res) => {
     const hash = req.body['hash']
     const filename = req.body['filename']
     const comment = req.body['comment']
 
-    res.send(`Hello World! ${hash} ${filename} ${comment}`)
+    const [nonce, signedTx, result] = await addEntry(hash, filename, comment)
+    const response = {
+        nonse: nonce,
+        signedTx: signedTx,
+        result: result
+    }
+    res.send(response)
 })
+
+async function addEntry(hashVal, filename, comment) {
+    var publicNonce = await web3.eth.getTransactionCount(fromAccountPublicKey, "pending");
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // creating call for addEntry() function
+    const addEntryFunctionArgs = web3.eth.abi
+        .encodeParameters(addEntryAbi.inputs, [hashVal, filename, comment])
+        .slice(2);
+    
+    var rawTx = {
+        nonce: web3.utils.numberToHex(publicNonce),
+        from: fromAccountPublicKey,
+        value: 0,
+        to: deployedContractAddress,
+        data: `${addEntryAbi.signature + addEntryFunctionArgs}`,
+        gasPrice: "0xFFFFFF",
+        gasLimit: "0xFFFFF",
+    };
+    var tx = new Tx(rawTx, { common: customCommon });
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    var privateKey = Buffer.from(fromAccountPrivateKey, "hex");
+    tx.sign(privateKey);
+    var serializedTx = tx.serialize();
+    var signedTx = `0x${serializedTx.toString("hex")}`;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    var result = await web3.eth.sendSignedTransaction(signedTx);
+
+    return [publicNonce, signedTx, result]
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
